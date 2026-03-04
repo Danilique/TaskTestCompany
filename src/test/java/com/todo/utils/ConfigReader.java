df973e9a578cc5b0c34a7189b86211ee4d1b3820
Daniil <daniil19969@gmail.com>
2026-03-04 14:02:28 +0200

    feat: add config management

---
+++ src/test/java/com/todo/utils/ConfigReader.java
@@ -0,0 +1,35 @@
+package com.todo.utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+
+    private static Properties properties;
+
+    static {
+        try {
+            FileInputStream file = new FileInputStream(
+                "src/test/resources/config.properties"
+            );
+            properties = new Properties();
+            properties.load(file);
+            file.close();
+        } catch (IOException e) {
+            throw new RuntimeException("config.properties file not found", e);
+        }
+    }
+
+    public static String get(String key) {
+        String value = properties.getProperty(key);
+        if (value == null) {
+            throw new RuntimeException("Property '" + key + "' not found in config.properties");
+        }
+        return value;
+    }
+
+    public static int getInt(String key) {
+        return Integer.parseInt(get(key));
+    }
+}
